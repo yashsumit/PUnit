878116c335b032b9c18fd2ccd0a91bce620729ad
yashsumit <sumit.joshi@yash.com>
2016-08-01 19:39:32 +0530

    Base Line

---
+++ ardalis-paralleltests-23cbd11122ca/SideEffectTester.cs
@@ -0,0 +1,47 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParallelTests
+{
+    public class Foo
+    {
+        public Foo() : this("Default Name")
+        {
+
+        }
+
+        public Foo(string Name)
+        {
+            this.Name = Name;
+        }
+
+        public string Name { get; set; }
+    }
+
+    [TestClass]
+    public class SideEffectTester
+    {
+        private static Foo _foo;
+
+        [ClassInitialize]
+        public static void SetUp(TestContext context)
+        {
+            _foo = new Foo();
+        }
+
+        [TestMethod]
+        public void FooNameShouldDefaultToDefaultName()
+        {
+            Thread.Sleep(100);
+            Assert.AreEqual("Default Name", _foo.Name);
+        }
+
+        [TestMethod]
+        public void FooNameShouldShowNewValueOnceSet()
+        {
+            _foo.Name = "New Name";
+            Thread.Sleep(100);
+            Assert.AreEqual("New Name", _foo.Name);
+        }
+    }
+}
