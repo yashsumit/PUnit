912f91730cd5d25c5110bace21b978e16db852f5
yashsumit <sumit.joshi@yash.com>
2016-08-02 05:44:46 -0700

    New Folder with File

---
+++ ardalis-paralleltests-23cbd11122ca/New folder/ParallelTester.cs
@@ -0,0 +1,130 @@
+﻿using System.Threading;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace ParallelTests
+{
+    [TestClass]
+    public partial class ParallelTester
+    {
+        [TestMethod]
+        public void Test01()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test02()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test03()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test04()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test05()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test06()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test07()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test08()
+        {
+            Thread.Sleep(26000);
+        }
+        [TestMethod]
+        public void Test09()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test10()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test11()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test12()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test13()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test14()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test15()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test16()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test17()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test18()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test19()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test20()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test21()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test22()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test23()
+        {
+            Thread.Sleep(50000);
+        }
+        [TestMethod]
+        public void Test24()
+        {
+            Thread.Sleep(50000);
+        }
+    }
+}
